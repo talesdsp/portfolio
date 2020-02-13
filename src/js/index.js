f2a8b5b57e96436c56f87fab20bd29729d4b848f
Tales Pereira <talesdsp@gmail.com>
2020-02-13 15:59:50 -0300

    refactored functions, new documentation,

--- src/js/index.js
+++ src/js/index.js
@@ -1,52 +1,81 @@
-window.addEventListener("load", () => {
-  const topNav = document.querySelector(".top-nav");
-  const menuToggler = document.querySelector(".menu-toggler");
-  const nav = document.querySelector(".nav-list");
+const topNav = document.querySelector(".top-nav");
+const menuToggler = document.querySelector(".menu-toggler");
+const nav = document.querySelector(".nav-list");
 
-  menuToggler.addEventListener("click", (e) => {
-    menuToggler.classList.toggle("open");
-    topNav.classList.toggle("open");
+menuToggler.addEventListener("click", toggleMenu);
+function toggleMenu(e) {
+  menuToggler.classList.toggle("open");
+  topNav.classList.toggle("open");
+}
+
+nav.addEventListener("click", closeMenu);
+function closeMenu(e) {
+  menuToggler.classList.remove("open");
+  topNav.classList.remove("open");
+}
+
+document.addEventListener("aos:in:custom-fade-in", setOpacity);
+
+function setOpacity({detail}) {
+  setTimeout(() => (detail.style.opacity = 1), 2500);
+}
+
+document.querySelectorAll('nav a[href^="#"]').forEach((v) => {
+  v.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    let t = document.querySelector(this.hash);
+    smooth(t, 800);
+    setTimeout(() => addFocus(t), 1000);
   });
-
-  nav.addEventListener("click", (e) => {
-    menuToggler.classList.remove("open");
-    topNav.classList.remove("open");
-  });
-
-  document.querySelectorAll('nav a[href="#"]').forEach((v) => {
-    document.querySelector("html").animate(
-      {
-        scrollTop: v.offsetTop - 400
-      },
-      {duration: 2000}
-    );
-  });
-
-  document.querySelector(".uplink").addEventListener("click", (e) => {
-    smooth(document.querySelector("#landing"), 1000);
-    setTimeout(() => addFocus(target), 1000);
-  });
-
-  document.querySelector("form #subject").addEventListener("input", sanitize);
-  document.querySelector("form #body").addEventListener("input", sanitize);
 });
 
+document.querySelector(".uplink").addEventListener("click", scrollTop);
+function scrollTop(e) {
+  smooth(document.querySelector("#landing"), 1000);
+  setTimeout(() => addFocus(document.querySelector("#landing")), 1000);
+}
+
+document.querySelector("form").addEventListener("submit", sendEmail);
+document.querySelector("form #subject").addEventListener("input", onChangeSubject);
+document.querySelector("form #body").addEventListener("input", onChangeBody);
+
+/**
+ * Smooth scroll function
+ * @param {Object} target - element to scroll to
+ * @param {Number} duration - transition time
+ */
 function smooth(target, duration) {
   let targetPosition = target.getBoundingClientRect().top;
   let startPosition = window.pageYOffset;
   let startTime = null;
 
+  /**
+   * The animation takes the currentTime, do the ease calculation, scroll to the result, recursively calls itself again with a different currentTime
+   * @param {Number} currentTime
+   */
   function animation(currentTime) {
     if (startTime === null) startTime = currentTime;
 
     let timeElapsed = currentTime - startTime;
-    let run = ease(timeElapsed, startPosition, targetPosition, duration);
+    let run = ease(
+      timeElapsed,
+      startPosition,
+      targetPosition - (window.innerWidth < 768 ? 0 : 200),
+      duration
+    );
 
     window.scrollTo(0, run);
 
     if (timeElapsed < duration) requestAnimationFrame(animation);
   }
 
+  /**
+   * Ease-in-quadratic formula
+   * @param {Number} t
+   * @param {Number} b
+   * @param {Number} c
+   * @param {Number} d
+   */
   function ease(t, b, c, d) {
     t /= d / 2;
     if (t < 1) return (c / 2) * t * t + b;
@@ -56,22 +85,40 @@ function smooth(target, duration) {
   window.requestAnimationFrame(animation);
 }
 
+/**
+ * Add focus to element after scroll, for accessibility purposes
+ * @param {Object} t - element
+ */
 function addFocus(t) {
-  t.nextSibling.focus();
-  if (document.activeElement === t.nextSibling) {
-    return false;
+  t.focus();
+  if (document.activeElement === t) {
+    return;
   } else {
-    t.nextSibling.setAttribute("tabindex", "-1"); // Adding tabindex for elements not focusable
-    t.nextSibling.focus(); // Set focus again
+    t.setAttribute("tabindex", "-1"); // Adding tabindex for elements not focusable
+    t.focus(); // Set focus again
   }
 }
 
-function sanitize(e) {
-  // e.target.value = escapeRegExp(e.target.value);
+/**
+ * Open user default email app with To, Subject, and Body, filled
+ * @param {Object} e - element
+ */
+function sendEmail(e) {
+  e.preventDefault();
+
+  window.location.href = `mailto:talesdsp@gmail.com?subject=${encodeURIComponent(
+    subject
+  )}&body=${encodeURIComponent(body)}`;
 }
 
-function escapeRegExp(text) {
-  // return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+let body;
+function onChangeBody(e) {
+  body = e.target.value;
+}
+
+let subject;
+function onChangeSubject(e) {
+  subject = e.target.value;
 }
 
 window.AOS.init({
